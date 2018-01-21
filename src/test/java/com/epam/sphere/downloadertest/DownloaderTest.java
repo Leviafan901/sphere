6fa3949f3ad92b8d9b0b0a305609552296900f27
Alex <leha_sosenkov@mail.ru>
2018-01-22 00:33:16 +0300

    GetRealResult

--- src/test/java/com/epam/sphere/downloadertest/DownloaderTest.java
+++ src/test/java/com/epam/sphere/downloadertest/DownloaderTest.java
@@ -2,10 +2,27 @@ package com.epam.sphere.downloadertest;
 
 import org.junit.Assert;
 import org.junit.Test;
- 
+import com.epam.projectsphere.downloader.Downloader;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DownloaderTest {
 
+	private Downloader downloader = new Downloader();
+	private static final String PATH = "D:\\test.txt";
+	private double[] testArray = {1.0, 2.0, 3.0};
+	private List<double[]> list = new ArrayList<double[]>();
+	
+	
+	@Test
+	public void shouldDownloadFileAndReturnListOfArraysDouble() throws FileNotFoundException {
+		
+		List<double[]> listOfArrays = downloader.downloadParameters(PATH);
+		list.add(testArray);
+		Assert.assertEquals(listOfArrays, list); 
+	}
+	
 }
